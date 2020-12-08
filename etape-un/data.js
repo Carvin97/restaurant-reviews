08f60a8554ba6160297b5d7142bf7299270ab8fe
Carvin97 <godblesscarvin@gmail.com>
2020-12-08 12:57:08 -0400

    Etape Un terminée

---
+++ etape-un/data.js
@@ -0,0 +1,52 @@
+const data = [
+    {
+       "restaurantName":"La Fringale",
+       "address":"Chemin de la Retraite, Baie-Mahault 97122, Guadeloupe",
+       "lat":16.241084,
+       "long":-61.596389,
+       "ratings":[
+          {
+             "stars":4,
+             "comment":"Très bonne pizza et bon accueil malgré le sous effectif du personnel. Merci beaucoup"
+          },
+          {
+             "stars":3,
+             "comment":"Loin d'être la meilleure pizza que j'ai mangé.  La pâte est très fine et pas croustillante. (Chacun ses goûts) . Le goût n'est pas relevé.. L'accueil est bien ."
+          }
+       ]
+    },
+
+    {
+       "restaurantName":"La Rhumerie du pirate",
+       "address":"Route de la Pointe des Châteaux Saint - François, 97118, Guadeloupe",
+       "lat":16.250620,
+       "long":-61.198929,
+       "ratings":[
+          {
+             "stars":5,
+             "comment":"Très bon accueil servive professionnel aux petits soins, plats excellent fait maison très bon rapport qualité/ prix je vous conseille de vous laisser tenter par un des rhums au comptoir très belle sélection N'hésitez pas à y aller"
+          },
+          {
+             "stars":4,
+             "comment":"Au bord de l'eau, vue sur Marie Galante, service agréable quoique un peu dans le jus. Cuisine créole de bonne qualité avec du choix, prix abordable."
+          }
+       ]
+    },
+
+    {
+        "restaurantName":"La Belle Chaudière",
+        "address":"Chemin De Ravine Chaude, Lamentin 97129, Guadeloupe",
+        "lat":16.234512,
+        "long":-61.663783,
+        "ratings":[
+           {
+              "stars":4,
+              "comment":"Petite découverte sympathique. Bien que quelques aprioris en arrivant sur les lieux; très vite l’ambiance, le décor ont joué le jeu et la nourriture était excellente ! 💥 Il faudrait cependant plus de choix au niveau des boissons !"
+           },
+           {
+              "stars":5,
+              "comment":"Nous avons faits le repas de baptême de mon fils le 15 juillet 2018. Ils nous fait une magnifique table nous ont accueilli avec douceur et gentillesse tout au long du repas. Le cadre est magnifique Le repas typiquement antillais étais délicieux"
+           }
+        ]
+    }
+]
